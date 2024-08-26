c4ddc9aa847f9330f579d524e20028934ea223a5
a-xitas <a.xitas@gmail.com>
2024-08-26 12:46:54 +0100

    File Inspection

---
+++ File Inspection-389.sh
@@ -0,0 +1,23 @@
+## 2. Head or Tail ##
+
+/home/dq$ cd rg_data
+
+## 3. Option-arguments ##
+
+/home/dq/rg_data$ tail -n 8 Arts
+
+## 4. Counting Lines ##
+
+/home/dq/rg_data$ answer 12 1469 20 737 17
+
+## 5. Pretty Printing ##
+
+/home/dq$ column -s':' -t characters
+
+## 6. File Sample ##
+
+/home/dq$ whatis suff
+
+## 7. Types of Files ##
+
+/home/dq/files$ file *
